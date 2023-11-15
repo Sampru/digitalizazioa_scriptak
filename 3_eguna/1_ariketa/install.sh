8f6e14b2fad93e75b0180a4618d89072e72e80d8
Asier Sampietro <asiersampietro@gmail.com>
2023-11-15 23:34:26 +0100

    Update install.sh

--- 3_eguna/1_ariketa/install.sh
+++ 3_eguna/1_ariketa/install.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# Honezkero konturatuko zinaten scriptak
+# ere irakurtzea badagoela.
+
+# Bertan jartzen dudana ./install.sh
+# egiterakoan zuen Raspberryetan ere
+# exekutatzen da.
+
+# Adibidez, hemengo hau eginez gero,
+# terminalak scripta exekutatu baino
+# lehen agurtu egingo gaitu.
+
+echo "Kaixo, scripta exekutatzera noa!"
+
+# Kodigoa irakurtzeari eta interpretatzeari
+# alderantzizko ingeniaritza deitzen zaio.
+# Hori izango da gaurko erronka.
+
+mkdir ~/3_eguna/asd/fgh/../jkl/
